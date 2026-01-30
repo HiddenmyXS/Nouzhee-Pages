a7cc8d663e9469642174095e916931e053e28b00
Muhammad Dhiya Altamis Mifzal Maula <dhiyaaltamis@gmail.com>
2026-01-31 00:05:17 +0700

    update v1.5 path 4

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+  output: "export"
 };
-
-module.exports = {
-  i18n: {
-    locales: ['en-US', 'id-ID', 'nl-NL'],
-    defaultLocale: 'en-US',
-    domains: [
-      {
-        domain: 'nouzhee.my.id/en',
-        defaultLocale: 'en-US',
-      },
-      {
-        domain: 'nouzhee.my.id/nl',
-        defaultLocale: 'nl-NL',
-      },
-      {
-        domain: 'nouzhee.my.id/id',
-        defaultLocale: 'id-ID',
-        http: true,
-      },
-    ],
-  },
-}
 
 export default nextConfig;
